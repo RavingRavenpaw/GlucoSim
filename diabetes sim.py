f4b095e1c7b1870a654d3274f3283476c726040d
RavinfRavenpaw <chromefusion44@gmail.com>
2015-12-12 18:09:11 -0500

    Started Work
    Mainly I'm figuring out what variables I'll need to account for blood
    sugar and general information of the body of the person that will be
    simulated.

--- diabetes sim.py
+++ diabetes sim.py
@@ -1,1 +1,45 @@
 #[placeholder]
+'''
+So basically, I need to implement a few things:
+    1. Primary Energy System/Glycolysis - Body uses ingested glucose from food.
+    
+    1.5 Liver Glycolysis - Body uses glycogen from the liver.
+    
+    2. Auxiliary Energy System/Ketosis - Body breaks down stored fat.
+    Doing too much of this will lead to ketoacidosis.
+    
+    3. Muscle breakdown (myosis?) - Body breaks down muscle tissue. Doing too
+    much of this will lead to severe atrophy of the muscles.
+    
+    4.Hormones - Effect how energy is used.
+'''
+
+def startSim():
+    #PATIENT INFO
+    #----------------------
+    #Sex: Female
+    #Weight: 156lb
+    #Body fat percentage: 10%
+    #Weight from fat: 15.6lb
+    #Body fat in grams: 7076g
+    
+    #
+    #
+    #
+    #Initialize the patient's virtual "body."
+    #These are not any specific unit, but are based off of how many
+    #grams of a substance is taken in per day or is usually in the body.
+    #
+    #Glucose assumes 275g of carb is ingested every day.
+    #Glycogen assumes about 100g of glycogen is stored in the liver.
+    #Insulin assumes about 10g of insulin is stored in the pancreas.
+    #Glucagon assumes about 10g of glucagon is stored in the pancreas.
+    #Fat assumes about 8% of
+    
+    glucose = 275
+    glycogen = 100
+    insulin = 10
+    glucagon = 10
+    fat = 7067
+    
+    
